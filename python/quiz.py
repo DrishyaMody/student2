c8e7e5513726d8e0f505834571cd8eb0374ec285
DrishyaMody <modydrishya@gmail.com>
2023-08-29 14:18:59 -0700

    commit

--- python/quiz.py
+++ python/quiz.py
@@ -5,9 +5,9 @@ score=0
 
 
 #Question 1 sequence
-question1=input('What python version should we be using?')
+question1=input('How many planets are in the solar system')
 print(f'{question1}')
-if question1 == '3.11.4':
+if question1 == '8':
     print('Great Job! Ready for the next question?')
     score= score +1
 else:
@@ -15,8 +15,35 @@ else:
      
 
 
-#Question 2 Sequqnce
-question2=input('If you see many lines of code in order, what would College Board call it?')
+#Question 2 Sequence
+question2=input('What sport does Lionel Messi play?')
 print(f'{question2}')
+if question2== 'soccer':
+    print('Great Job! Ready for the last question?')
+    score=score+1
+elif question2== 'Soccer':
+    print('Great Job! Ready for the last question?')
+    score=score+1
+else: 
+     print ('Sorry that is incorrect')
+
+
+#Question 3 sequence
+question3=input('How many continents are there?')
+if question3=='7':
+    print('Great Job! Ready for your score?')
+    score=score+1
+else:
+    print ('Sorry that is incorrect')
+
+
+#Score/percentage calculation
+percentage = (score/3)*100
+
+print (f'Your score is {percentage}%')
+
+
+
+
 
 
